ae59abcbe7ccb31321fd9963566c1c110cf43dca
Kauanrodrigues01m <kauanrl09@gmail.com>
2024-12-21 00:20:56 -0300

    resgister Category model in admin panel

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at', 'updated_at')
+    
+    
+admin.site.register(Category, CategoryAdmin)
