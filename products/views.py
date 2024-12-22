2825ecbcaf5a794cb99d0a0e038e2794bbdc383f
Kauanrodrigues01m <kauanrl09@gmail.com>
2024-12-22 18:05:37 -0300

    feat: added CRUD for products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from brands.models import Brand
+from categories.models import Category
+from .models import Product
+from .forms import ProductForm
 
-# Create your views here.
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/products_list.html'
+    context_object_name = 'products'
+    paginate_by = 10
+    permission_required = 'products.view_product'
+
+    def get_queryset(self):
+        queryset = Product.objects.select_related('category', 'brand').all()
+        title = self.request.GET.get('title')
+        serie_number = self.request.GET.get('serie_number')
+        category = self.request.GET.get('category')
+        brand = self.request.GET.get('brand')
+        
+        filters = {}
+
+        if title:
+            filters['title__icontains'] = title
+        if serie_number:
+            filters['serie_number__icontains'] = serie_number
+        if category:
+            filters['category__id'] = category
+        if brand:
+            filters['brand__id'] = brand
+            
+        if filters:
+            queryset = queryset.filter(**filters)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['brands'] = Brand.objects.all()
+        context['page_product_is_active'] = 'active'
+        print(context['categories'])
+        return context
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'products/product_create.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('products:products_list')
+    permission_required = 'products.add_product'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_product_is_active'] = 'active'
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+    permission_required = 'products.view_product'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_product_is_active'] = 'active'
+        return context
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'products/product_update.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('products:products_list')
+    permission_required = 'products.change_product'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_product_is_active'] = 'active'
+        return context
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'products/product_delete.html'
+    success_url = reverse_lazy('products:products_list')
+    permission_required = 'products.delete_product'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_product_is_active'] = 'active'
+        return context
