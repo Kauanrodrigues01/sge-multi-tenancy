1439f9418cd49d1017e436145fbdf7b79147c5b5
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-01-02 00:37:30 -0300

    remove django-jazzmin

--- app/settings.py
+++ app/settings.py
@@ -32,9 +32,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
-THIRD_PARTY_APPS = [
-    # 'jazzmin',
-]
+THIRD_PARTY_APPS = []
 
 LOCAL_APPS = [
     'brands',
@@ -158,39 +156,6 @@ LOGIN_URL = 'authentication:login'
 
 if DEBUG:
     INSTALLED_APPS += ['django_extensions']
-    
-JAZZMIN_UI_TWEAKS = {
-    "navbar_small_text": False,
-    "footer_small_text": False,
-    "body_small_text": True,
-    "brand_small_text": False,
-    "brand_colour": False,
-    "accent": "accent-primary",
-    "navbar": "navbar-secondary navbar-dark",
-    "no_navbar_border": False,
-    "navbar_fixed": True,
-    "layout_boxed": False,
-    "footer_fixed": True,
-    "sidebar_fixed": True,
-    "sidebar": "sidebar-dark-info",
-    "sidebar_nav_small_text": False,
-    "sidebar_disable_expand": False,
-    "sidebar_nav_child_indent": False,
-    "sidebar_nav_compact_style": True,
-    "sidebar_nav_legacy_style": False,
-    "sidebar_nav_flat_style": True,
-    "theme": "superhero",
-    "dark_mode_theme": None,
-    "button_classes": {
-        "primary": "btn-primary",
-        "secondary": "btn-secondary",
-        "info": "btn-info",
-        "warning": "btn-warning",
-        "danger": "btn-danger",
-        "success": "btn-success"
-    },
-    "actions_sticky_top": False
-}
 
 
 LOGGING = {
