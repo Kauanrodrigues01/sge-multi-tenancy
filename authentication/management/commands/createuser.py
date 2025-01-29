2d9738079a3e10542aca2fb4945bb9558973551c
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-01-29 10:23:48 -0300

    feat(django): add createuser and wait_for_db management commands
    - Added `createuser` command to initialize the application's default user
    - Implemented `wait_for_db` command to ensure the database is ready before starting the application
    - Improved application setup by automating user creation and database availability checks

---
+++ authentication/management/commands/createuser.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from decouple import config
+
+SUPERUSER_USERNAME = config('SUPERUSER_USERNAME', default='admin', cast=str)
+SUPERUSER_EMAIL = config('SUPERUSER_EMAIL', default='admin@gmail.com', cast=str)
+SUPERUSER_PASSWORD = config('SUPERUSER_PASSWORD', default='admin', cast=str)
+
+class Command(BaseCommand):
+    help = 'Create a superuser'
+
+    def handle(self, *args, **kwargs):
+        user, created = User.objects.get_or_create(
+            username=SUPERUSER_USERNAME,
+            email=SUPERUSER_EMAIL
+        )
+        
+        if created:
+            user.set_password(SUPERUSER_PASSWORD)
+            user.is_superuser = True
+            user.is_staff = True
+            user.save()
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
+            
